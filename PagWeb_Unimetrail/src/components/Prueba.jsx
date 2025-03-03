17ab24fa86279f40b21efcd0a69e5907a3bc80d1
r1c4rd0-b43t4 <155971283+r1c4rd0-b43t4@users.noreply.github.com>
2025-03-03 02:13:58 -0400

    plantilla funcional ahora si
    ya tiene el tailwind bien y el react

---
+++ PagWeb_Unimetrail/src/components/Prueba.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Prueba() {
+  return (
+    <div className="bg-green-500 text-black p-4 rounded-lg">
+      <h1 className="text-2xl font-bold">Hola, Tailwind CSS!</h1>
+      <p className="mt-2">Este es un componente de prueba utilizando Tailwind CSS.</p>
+    </div>
+  );
+}
