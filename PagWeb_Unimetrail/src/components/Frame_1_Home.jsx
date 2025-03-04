d98d99cb64267e158eb4cd7b4f196d525b597803
r1c4rd0-b43t4 <155971283+r1c4rd0-b43t4@users.noreply.github.com>
2025-03-04 18:04:46 -0400

    subo para probar footer

--- PagWeb_Unimetrail/src/components/Frame_1_Home.jsx
+++ PagWeb_Unimetrail/src/components/Frame_1_Home.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import BotonPrimario from './BotonPrimario'
-import ImagenAvila from '../assets/Imagen_Avila.png'
+import React from 'react';
+import BotonPrimario from './BotonPrimario';
+import ImagenAvila from '../assets/Imagen_Avila.png';
+import Avilabg from "../assets/avila_bg_f1_h.svg";
 
 export default function Frame_1_Home() {
     return (
-        <div className="flex justify-center items-center min-h-screen pl-10">
-            <div className="container py-30 px-30 flex flex-row items-center">
-                <div className="text-left w-1/2 mr-auto">
-                    <p className=''>Descubre nuevas aventuras</p>
-                    <h1 className="text-7xl font-bold mb-4 w-2/3">
+        <div className='w-screen h-auto'>
+            <div className="">
+                <div className="px-20 py-10 justify-center items-center">
+                    <p className='text-[#2A8F84] mb-2'>Descubre nuevas aventuras</p>
+                    <h1 className="text-6xl  font-bold mb-4">
                         <span style={{ color: '#00796B' }}>Bienvenido a</span> <span style={{ color: '#D76411' }}>Unimetrail</span>
                     </h1>
-                    <p className="text-2sm mb-5 w-3/4">Para aquellos estudiantes de la Unimet que desean descubrir nuevas formas de reconectar con el senderismo.</p>
-                    <BotonPrimario text='Explora'/>
-                </div>
-                <div className="w-1/2">
-                    <img src={ImagenAvila} className="w-full h-auto" />
+                    <p className="text-sm mb-5">Para aquellos estudiantes de la Unimet que desean descubrir nuevas formas de reconectar con el senderismo.</p>
+                    <BotonPrimario text="Descubre"/>
                 </div>
             </div>
+            <div>
+                <img src={ImagenAvila} className="absolute bottom-0 right-0" />
+            </div>
+            <img src={Avilabg} className="absolute bottom-0 left-0 w-auto h-auto opacity-50" />
         </div>
     )
 }
+
