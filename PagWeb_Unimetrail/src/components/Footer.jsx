587b44681f2091fca9661cb994679716a675a629
Aida Cardenas <aida.cardenas@correo.unimet.edu.ve>
2025-03-04 15:15:06 -0400

    Eliminado flex gigante
    saludos

--- PagWeb_Unimetrail/src/components/Footer.jsx
+++ PagWeb_Unimetrail/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+
 import BotonFooter from "./BotonFooter";
 import InstagramIcon from '../assets/instagram.svg';
 import FacebookIcon from '../assets/facebook.svg';
@@ -17,44 +18,37 @@ export default function Footer() {
     ];
 
     return (
-        <div className="flex flex-col min-h-screen">
-            <main className="flex-grow">
-                {/* Contenido principal */}
-            </main>
-            <footer className="bg-teal-700 text-white py-6 w-screen">
-                <div className="container mx-auto px-6 w-full">
-                    <div className="flex flex-col md:flex-row justify-between items-center w-full">
-                        <div className="text-lg font-semibold">UnimeTrail</div>
-                        <nav className="flex flex-wrap justify-center md:justify-start space-x-6 text-sm">
-                            {upperLinks.map((link, index) => (
-                                <BotonFooter key={index} text={link.text} link={link.link} />
-                            ))}
-                        </nav>
-                        <div className="flex space-x-4 mt-4 md:mt-0">
-                            <a href="#" className="text-xl hover:underline">
-                                <img src={FacebookIcon} alt="Facebook" className="w-6 h-6" />
-                            </a>
-                            <a href="#" className="text-xl hover:underline">
-                                <img src={InstagramIcon} alt="Instagram" className="w-6 h-6" />
-                            </a>
-                        </div>
-                    </div>
-                    <hr className="my-4 border-gray-400 w-full" />
-                    <div className="flex flex-col md:flex-row justify-between items-center text-xs w-full">
-                        <div className="flex items-center space-x-2">
-                            <a href="#" className="text-xl hover:underline">
-                                <img src={UnimetIcon} alt="Unimet" className="w-12 h-12" />
-                            </a>
-                            <p>© 2025 UnimeTrail. All rights reserved.</p>
-                        </div>
-                        <nav className="flex flex-wrap justify-center md:justify-start space-x-6 text-sm mt-4 md:mt-0 mx-auto w-1/2 p-4">
-                            {lowerLinks.map((link, index) => (
-                                <BotonFooter key={index} text={link.text} link={link.link} />
-                            ))}
-                        </nav>
+        <footer className="bg-teal-700 text-white py-6 w-screen">
+            <div className="container mx-auto px-6 w-full">
+                <div className="flex flex-col md:flex-row justify-between items-center w-full">
+                    <div className="text-lg font-semibold">UnimeTrail</div>
+                    <nav className="flex flex-wrap justify-center md:justify-start space-x-6 text-sm">
+                        {upperLinks.map((link, index) => (
+                            <BotonFooter key={index} text={link.text} link={link.link} />
+                        ))}
+                    </nav>
+                    <div className="flex space-x-4 mt-4 md:mt-0">
+                        <a href="#" className="text-xl hover:underline">
+                            <img src={FacebookIcon} alt="Facebook" className="w-6 h-6" />
+                        </a>
+                        <a href="#" className="text-xl hover:underline">
+                            <img src={InstagramIcon} alt="Instagram" className="w-6 h-6" />
+                        </a>
                     </div>
                 </div>
-            </footer>
-        </div>
+                <hr className="my-4 border-gray-400 w-full" />
+                <div className="flex flex-col md:flex-row justify-between items-center text-xs w-full">
+                    <div className="flex items-center space-x-2">
+                    <a href="#" className="text-xl hover:underline"> <img src={UnimetIcon} alt="Unimet" className="w-12 h-12" /> </a>
+                        <p>© 2025 UnimeTrail. All rights reserved.</p>
+                    </div>
+                    <nav className="flex flex-wrap justify-center md:justify-start space-x-6 text-sm mt-4 md:mt-0 mx-auto w-1/2 p-4">
+                        {lowerLinks.map((link, index) => (
+                            <BotonFooter key={index} text={link.text} link={link.link} />
+                        ))}
+                    </nav>
+                </div>
+            </div>
+        </footer>
     );
 }
