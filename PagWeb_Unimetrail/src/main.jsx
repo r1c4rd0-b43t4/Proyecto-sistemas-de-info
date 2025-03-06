56031576abd7bb941da175f239663707c269ee4e
r1c4rd0-b43t4 <155971283+r1c4rd0-b43t4@users.noreply.github.com>
2025-03-06 00:16:58 -0400

    cambio de main

--- PagWeb_Unimetrail/src/main.jsx
+++ PagWeb_Unimetrail/src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home.jsx'
-import { DatosRuta } from './components/DatosRuta.jsx'
-import { TarjetaRuta } from './components/TarjetaRuta.jsx'
-import {DescubreRutas} from './components/DescubreRutas.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DescubreRutas/>
+    <Home/>
   </StrictMode>,
 )
