cbdada070f64372496fd655af2f9748863b80a12
r1c4rd0-b43t4 <155971283+r1c4rd0-b43t4@users.noreply.github.com>
2025-03-07 17:38:10 -0400

    paginas login y register

---
+++ PagWeb_Unimetrail/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header_NoSession from '../components/Header_NoSession';
+import Footer from '../components/Footer';
+
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <Header_NoSession/>
+      <Footer/>
+    </div>
+  );
+}
+
