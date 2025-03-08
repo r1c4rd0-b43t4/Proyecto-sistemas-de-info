4723fa8aa9be4e648a19cda65fb40fecea43abdf
r1c4rd0-b43t4 <155971283+r1c4rd0-b43t4@users.noreply.github.com>
2025-03-07 22:19:33 -0400

    upsi daisy ca,mbie el home

--- PagWeb_Unimetrail/src/main.jsx
+++ PagWeb_Unimetrail/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login/>
+    <Home/>
   </StrictMode>,
 )
