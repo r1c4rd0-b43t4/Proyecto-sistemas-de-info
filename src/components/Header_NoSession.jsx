00bb977f26d841da6266b8578bb8e612c2a0c8ec
r1c4rd0-b43t4 <155971283+r1c4rd0-b43t4@users.noreply.github.com>
2025-03-09 19:50:23 -0400

    Update Header_NoSession.jsx

--- src/components/Header_NoSession.jsx
+++ src/components/Header_NoSession.jsx
@@ -28,8 +28,8 @@ export default function Header_NoSession() {
 
         {/* botones incicio y registro */}
         <div className="hidden md:flex items-center gap-4">
-          <BotonPrimario text="Iniciar Sesión" />
-          <BotonSecundario text="Registrarse" />
+          <BotonPrimario text="Iniciar Sesión" to="/login" />
+          <BotonSecundario text="Registrarse" to="/register"/>
         </div>
 
         {/* icono menu hamburguesa */}
@@ -50,8 +50,8 @@ export default function Header_NoSession() {
         </button>
 
         <NavLinks />
-        <BotonPrimario text="Iniciar Sesión" />
-        <BotonSecundario text="Registrarse" />
+        <BotonPrimario text="Iniciar Sesión" to="/login"/>
+        <BotonSecundario text="Registrarse" to="/register"/>
       </div>
     </div>
   );
