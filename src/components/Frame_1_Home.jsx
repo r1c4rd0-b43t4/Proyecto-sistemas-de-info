73649b1f7a99ee2f98dacae66be1490c2cbeec6f
Aida Cardenas <aida.cardenas@correo.unimet.edu.ve>
2025-03-09 20:33:07 -0400

    texto arreglao siu

--- src/components/Frame_1_Home.jsx
+++ src/components/Frame_1_Home.jsx
@@ -10,13 +10,14 @@ export default function Frame_1_Home() {
     return (
         <div className='relative w-screen h-screen flex flex-col md:flex-row items-center justify-center md:justify-start'>
             <div className="relative z-10 md:w-1/2 p-10 text-center md:text-left">
-                <p className='text-[#2A8F84] mb-2 text-lg md:text-xl'>Descubre nuevas aventuras</p>
-                <h1 className="text-5xl md:text-7xl font-bold mb-4">
+                <p className='text-[#2A8F84] mb-2 text-base md:text-lg'>Descubre nuevas aventuras</p>
+                <h1 className="text-6xl md:text-8xl font-bold mb-4">
                     <span style={{ color: '#00796B' }}>Bienvenido a</span> <span style={{ color: '#D76411' }}>Unimetrail</span>
                 </h1>
                 <p className="text-lg md:text-xl text-[#2A8F84] mb-5">Para aquellos estudiantes de la Unimet que desean descubrir nuevas formas de reconectar con el senderismo.</p>
                 <BotonPrimario text="Descubre"/>
             </div>
+            
             {/* Avila de fondo cuando son dispositivos medianos o pequenos */}
             <div className="absolute inset-0 w-full h-full md:hidden">
                 <img src={ImagenAvila} className="w-full h-full object-cover opacity-30" alt="Fondo Avila" />
