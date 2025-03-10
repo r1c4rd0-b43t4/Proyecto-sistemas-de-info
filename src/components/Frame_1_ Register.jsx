14c751029809e442c007f9730a9db60cd8536ea7
r1c4rd0-b43t4 <155971283+r1c4rd0-b43t4@users.noreply.github.com>
2025-03-09 20:28:04 -0400

    login y register ya se ven

--- src/components/Frame_1_ Register.jsx
+++ src/components/Frame_1_ Register.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
 import BotonPrimario from './BotonPrimario';
 import ImagenAvila from '../assets/Imagen_Avila.svg';
 import Input from "./Input_V1"; 
 import Underline from "../assets/Underline_1.svg"
-import { useNavigation } from 'react-router';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import { app } from "../../credentials.js" //puede que de error
+import { app } from "../../credentials.js";
 const auth = getAuth(app);
 
 export default function Frame_1_Home() {
-
-    const navigation = useNavigation();
+    const navigate = useNavigate();
     const [name, setName] = useState("");
     const [lastName, setLastName] = useState("");   
     const [email, setEmail] = useState("");
@@ -26,36 +25,32 @@ export default function Frame_1_Home() {
         try {        
             setLoading(true);
             const nombreRegistrado = await createUserWithEmailAndPassword(auth, email, password)
-
             console.log(nombreRegistrado.user.email)
-    
             setEmail("");
             setPassword("");    
             setName("");
             setLastName("");
             setPhone("");
-            setLoading(false);
-            navigation("/")
-            
+            navigate("/");
         } catch (error) {
-            console.log(error)
-            if (error.code === "Firebase: Error (auth/email-already-in-use).") {
+            console.log(error);
+            if (error.code === "auth/email-already-in-use") {
                 setError("El correo ya está en uso");
+            } else {
+                setError(error.message);
             }
-            setError(error.message);
+        } finally {
+            setLoading(false);
         }
-
-
     }
-
 
     return (
         <div className='relative w-screen h-screen flex pt-5'>
-            (loading && <div className="">Cargando...</div>)
-            (error && <div className="">Error: {error}</div>)
+            {loading && <div className="">Cargando...</div>}
+            {error && <div className="">Error: {error}</div>}
             <div className="flex justify-center items-center h-full w-1/2">
                 <div className="flex justify-center items-center flex-col space-y-3">
-                    <form onSubmit={handleRegister}>
+                    <form onSubmit={handleRegister} className="flex justify-center items-center flex-col space-y-3 pt-6">
                         <h1 className="text-4xl font-bold">
                             <span className="text-[#00796B]">Registrarse en </span><span className="text-[#D76411]">Unimetrail</span>
                         </h1>
@@ -63,23 +58,24 @@ export default function Frame_1_Home() {
                             <img src={Underline} alt="Underline" className=""/>
                         </div>
                         <div className='space-y-3 w-full'>
-                        <Input titulo="Nombre" placeholder="Ingresa tu nombre" type="text" name="nombre" value={name} onChange={(e) => setName(e.target.value)}/>
-                        <Input titulo="Apellido" placeholder="Ingresa tu apellido" type="text" name="apellido" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-                        <Input titulo="Correo" placeholder="Ingresa tu correo unimet" type="email" name="email"  value={email} onChange={(e) => setEmail(e.target.value)}/>
-                        <Input titulo="Contraseña" placeholder="Ingresa tu contraseña" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                        {/* <Input titulo="Repetir contraseña" placeholder="Ingresa tu contraseña nuevamente" type="text" name="nombre"/> */}
-                        <Input titulo="Número celular" placeholder="0412111111" type="tel" name="telefono" value={phone} onChange={(e) => setPhone(e.target.value)}/>
+                            <Input titulo="Nombre" placeholder="Ingresa tu nombre" type="text" name="nombre" value={name} onChange={(e) => setName(e.target.value)}/>
+                            <Input titulo="Apellido" placeholder="Ingresa tu apellido" type="text" name="apellido" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+                            <Input titulo="Correo" placeholder="Ingresa tu correo unimet" type="email" name="email"  value={email} onChange={(e) => setEmail(e.target.value)}/>
+                            <Input titulo="Contraseña" placeholder="Ingresa tu contraseña" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                            {/* <Input titulo="Repetir contraseña" placeholder="Ingresa tu contraseña nuevamente" type="text" name="nombre"/> */}
+                            <Input titulo="Número celular" placeholder="0412111111" type="tel" name="telefono" value={phone} onChange={(e) => setPhone(e.target.value)}/>
                         </div>
                         <BotonPrimario text="Registrarse" type="submit" className="mt-4 w-full" /> 
                     </form>
                     <p>
-                        <span className="text-[#00796B]">¿Ya tienes una cuenta? </span><span className="text-[#005147]"><Link to="/login">Iniciar Sesión </Link></span>
+                        <span className="text-[#00796B]">¿Ya tienes una cuenta? </span><span className="text-[#005147]"><Link to="/login">Iniciar Sesión</Link></span>
                     </p>
                 </div>
                 <div>
-                <img src={ImagenAvila} alt="Fondo montaña" className="absolute bottom-0 right-0 w-1/2 h-full object-cover"/>
+                    <img src={ImagenAvila} alt="Fondo montaña" className="absolute bottom-0 right-0 w-1/2 h-full object-cover"/>
                 </div>
             </div>
         </div>
-    )
+    );
 }
+
--- src/components/Logo_Responsive.jsx
+++ src/components/Logo_Responsive.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import property1DefaultLogo from "../assets/Property_1=Default_Logo.svg";
 import property1Variant2Logo from "../assets/Property_1=Variant2_Logo.svg";
 import property1Variant3Logo from "../assets/Property_1=Variant3_Logo.svg";
+import { Link } from "react-router";
 
 // Quite el position absolute porque no se necesita juasjuas
 export const LogoResponsive = ({ className }) => {
@@ -25,15 +26,17 @@ export const LogoResponsive = ({ className }) => {
     };
 
     return (
-        <img
-            className={`w-[150px] md:w-[202px] max-w-[180px] md:max-w-[202px] ${className}`}
-            alt="Logo Unimetrail"
-            src={currentLogo}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            onMouseDown={handleMouseDown}
-            onMouseUp={handleMouseUp}
-        />
+        <Link to="/">
+            <img
+                className={`w-[150px] md:w-[202px] max-w-[180px] md:max-w-[202px] ${className}`}
+                alt="Logo Unimetrail"
+                src={currentLogo}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+            />
+        </Link>
     );
 };
 
